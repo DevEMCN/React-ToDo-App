4ddf5aedf0a375ac8161381e33482f6390b64efb
DevEMCN <devemcn@gmail.com>
2016-05-31 16:14:26 +0100

    style todo application

--- app/components/ToDo.jsx
+++ app/components/ToDo.jsx
@@ -4,7 +4,7 @@ var moment = require('moment');
 var ToDo = React.createClass({
   render: function() {
     var {id, text, completed, createdAt, completedAt} = this.props;
-
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
       var message = "created ";
       var timestamp = createdAt;
@@ -18,10 +18,13 @@ var ToDo = React.createClass({
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
     return (
-      <div onClick = {() => {this.props.onToggle(id)}}>
-        <input type="checkbox" checked={completed}/>
-        <p>{text}</p>
-        <p>{renderDate()}</p>
+      <div className={todoClassName} onClick = {() => {this.props.onToggle(id)}}>
+        <div><input type="checkbox" checked={completed}/></div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
+
       </div>
     );
   }
--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -59,9 +59,17 @@ var ToDoApp = React.createClass({
 
     return (
       <div>
-        <ToDoSearch onSearch={this.handleSearch}/>
-        <ToDoList todos={filteredTodos} onToggle={this.handleToggle}/>
-        <AddToDo onAddToDo={this.handleAddTodo}/>
+        <h1 className="page-title">ToDo App</h1>
+
+        <div className="row">
+          <div className="column small-centered small-11 medium-6 large-5">
+            <div className="container">
+              <ToDoSearch onSearch={this.handleSearch}/>
+              <ToDoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <AddToDo onAddToDo={this.handleAddTodo}/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
